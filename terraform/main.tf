81de5841a3ac77c253b155edfa32e30a158cdb39
PathornKiti <pathorn.kiti@gmail.com>
2024-03-25 19:41:13 +0700

    update tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,3 +34,52 @@ resource "google_bigquery_dataset" "demo_dataset" {
   dataset_id = var.bq_dataset_name
   location   = var.location
 }
+
+resource "google_dataproc_cluster" "demo_cluster" {
+    name    = "${var.cluster_name}"
+    project = "${var.project}"
+    region  = "${var.region}"
+
+    labels  = "${var.labels}"
+
+    cluster_config {
+        delete_autogen_bucket = "${var.delete_autogen_bucket}"
+
+        staging_bucket        = "${var.staging_bucket}"
+
+        master_config {
+            num_instances     = "${var.master_num_instances}"
+            machine_type      = "${var.master_machine_type}"
+            disk_config {
+                boot_disk_size_gb = "${var.master_boot_disk_size_gb}"
+            }
+        }
+
+        worker_config {
+            num_instances     = "${var.worker_num_instances}"
+            machine_type      = "${var.worker_machine_type}"
+            disk_config {
+                boot_disk_size_gb = "${var.worker_boot_disk_size_gb}"
+                num_local_ssds    = "${var.worker_num_local_ssds}"
+            }
+        }
+
+        preemptible_worker_config {
+            num_instances     = "${var.preemptible_num_instances}"
+        }
+
+
+        software_config {
+            image_version       = "${var.image_version}"
+            override_properties = {
+
+            }
+        }
+
+        gce_cluster_config {
+            zone    = "${data.google_compute_zones.available.names[0]}"
+
+        }
+
+    }
+}
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -35,3 +35,67 @@ variable "bq_dataset_name" {
   description = "My BigQuery Dataset Name"
   default     = "airbnb_bkk_warehouse"
 }
+
+variable "cluster_name" {
+    description = "The name of the cluster, unique within the project and zoneData"
+}
+
+variable "labels" {
+    description = "The list of labels (key/value pairs) to be applied to instances in the cluster"
+    default     = {}
+}
+
+variable "delete_autogen_bucket" {
+    description = "If this is set to true, upon destroying the cluster, if no explicit staging_bucket was specified (i.e. an auto generated bucket was relied upon) then this auto generated bucket will also be deleted as part of the cluster destroy"
+    default     = "false"
+}
+
+variable "staging_bucket" {
+    description = "The Cloud Storage staging bucket used to stage files, such as Hadoop jars, between client machines and the cluster"
+    default =   ""
+}
+
+variable "master_num_instances" {
+    description = "Specifies the number of master nodes to create"
+    default     = 1
+}
+
+variable "master_machine_type" {
+    description = "The name of a Google Compute Engine machine type to create for the master"
+    default     = "n1-standard-4"
+}
+
+variable "master_boot_disk_size_gb" {
+    description = "Size of the primary disk attached to each node, specified in GB"
+    default     = 10
+}
+
+variable "worker_num_instances" {
+    description = "Specifies the number of worker nodes to create"
+    default     = 2
+}
+
+variable "worker_machine_type" {
+    description = "The name of a Google Compute Engine machine type to create for the worker nodes"
+    default     = "n1-standard-4"
+}
+
+variable "worker_boot_disk_size_gb" {
+    description = "Size of the primary disk attached to each worker node, specified in GB"
+    default     = 10
+}
+
+variable "worker_num_local_ssds" {
+    description = "The amount of local SSD disks that will be attached to each worker cluster node"
+    default     = 0
+}
+
+variable "preemptible_num_instances" {
+    description = "Specifies the number of preemptible nodes to create"
+    default     = 0
+}
+
+variable "image_version" {
+    description = "The Cloud Dataproc image version to use for the clustere"
+    default     = "1.2"
+}
